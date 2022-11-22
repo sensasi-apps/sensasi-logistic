7f29dbff6c40cf06d9198aa7c5784ac93dbdf0c1
Zain Adam <zainadam.id@gmail.com>
2022-11-22 18:09:47 +0700

    refactor: move view table to folder

--- app/Models/MaterialInDetail.php
+++ app/Models/MaterialInDetail.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Views\MaterialInDetailsStockView;
 
 class MaterialInDetail extends Model
 {
--- app/Models/Views/MaterialInDetailsStockView.php
+++ app/Models/Views/MaterialInDetailsStockView.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Views;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
