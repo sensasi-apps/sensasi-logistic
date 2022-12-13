677f08eb1e6d18f336b43ef2c89ac0f76e40aac0
Zain Adam <zainadam.id@gmail.com>
2022-12-13 20:12:31 +0700

    chore: add report nav

--- resources/views/layouts/components/_sidebar-menu-ul.blade.php
+++ resources/views/layouts/components/_sidebar-menu-ul.blade.php
@@ -2,42 +2,68 @@
     <li>
         <a class="nav-link" href="{{ url('dashboard') }}">
             <i class="fas fa-chart-line"></i>
-            <span>Dasbor</span>
+            <span>{{ __('Dashboard') }}</span>
         </a>
     </li>
 
     <li class="menu-header">Data</li>
 
     <li>
-        <a class="nav-link" href="{{ url('products') }}">
-            <i class="fas fa-box"></i>
-            <span>Produk</span>
+        <a class="nav-link" href="{{ url('materials') }}">
+            <i class="fas fa-pallet"></i>
+            <span>{{ __('Materials') }}</span>
         </a>
     </li>
 
     @if (Auth::user()->hasRole('Manufacturer|Super Admin'))
         <li>
-            <a class="nav-link" href="{{ url('batches') }}">
+            <a class="nav-link" href="{{ url('manufactures') }}">
                 <i class="fas fa-boxes"></i>
-                <span>Batch</span>
+                <span>{{ __('Manufactures') }}</span>
             </a>
         </li>
     @endif
 
     <li>
-        <a class="nav-link" href="{{ url('materials') }}">
-            <i class="fas fa-pallet"></i>
-            <span>Bahan</span>
+        <a class="nav-link" href="{{ url('products') }}">
+            <i class="fas fa-box"></i>
+            <span>{{ __('Products') }}</span>
         </a>
     </li>
 
+
+
+    <li class="menu-header">{{ __('Report') }}</li>
+
+    <li>
+        <a class="nav-link" href="{{ url('report/materials') }}">
+            <i class="fas fa-file-alt"></i>
+            <span>{{ __('Materials') }}</span>
+        </a>
+    </li>
+
+    <li>
+        <a class="nav-link" href="{{ url('report/manufactures') }}">
+            <i class="fas fa-file-alt"></i>
+            <span>{{ __('Manufactures') }}</span>
+        </a>
+    </li>
+
+    <li>
+        <a class="nav-link" href="{{ url('report/products') }}">
+            <i class="fas fa-file-alt"></i>
+            <span>{{ __('Products') }}</span>
+        </a>
+    </li>
+
+
     @if (Auth::user()->hasRole('Super Admin'))
-        <li class="menu-header">Sistem</li>
+        <li class="menu-header">{{ __('System') }}</li>
 
         <li>
             <a class="nav-link" href="{{ url('system/user') }}">
                 <i class="fas fa-user-cog"></i>
-                <span>Pengguna</span>
+                <span>{{ __('Users') }}</span>
             </a>
         </li>
 
