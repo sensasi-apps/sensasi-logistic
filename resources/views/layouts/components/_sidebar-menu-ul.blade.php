7df6e64ea3a89f5d43a20da39d3c73068e366a79
Zain Adam <zainadam.id@gmail.com>
2022-12-25 03:37:45 +0700

    chore: add active class on active menu

--- resources/views/layouts/components/_sidebar-menu-ul.blade.php
+++ resources/views/layouts/components/_sidebar-menu-ul.blade.php
@@ -1,5 +1,5 @@
 <ul class="sidebar-menu">
-    <li>
+    <li class="{{ request()->is('dashboard*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('dashboard') }}">
             <i class="fas fa-chart-line"></i>
             <span>{{ __('Dashboard') }}</span>
@@ -8,7 +8,7 @@
 
     <li class="menu-header">Data</li>
 
-    <li>
+    <li class="{{ request()->is('materials*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('materials') }}">
             <i class="fas fa-pallet"></i>
             <span>{{ __('Materials') }}</span>
@@ -16,7 +16,7 @@
     </li>
 
     @if (Auth::user()->hasRole('Manufacturer|Super Admin'))
-        <li>
+        <li class="{{ request()->is('manufactures*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ url('manufactures') }}">
                 <i class="fas fa-boxes"></i>
                 <span>{{ __('Manufactures') }}</span>
@@ -24,7 +24,7 @@
         </li>
     @endif
 
-    <li>
+    <li class="{{ request()->is('products*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('products') }}">
             <i class="fas fa-box"></i>
             <span>{{ __('Products') }}</span>
@@ -35,21 +35,21 @@
 
     <li class="menu-header">{{ __('Report') }}</li>
 
-    <li>
+    <li class="{{ request()->is('report/materials*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('report/materials') }}">
             <i class="fas fa-file-alt"></i>
             <span>{{ __('Materials') }}</span>
         </a>
     </li>
 
-    <li>
+    <li class="{{ request()->is('report/manufactures*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('report/manufactures') }}">
             <i class="fas fa-file-alt"></i>
             <span>{{ __('Manufactures') }}</span>
         </a>
     </li>
 
-    <li>
+    <li class="{{ request()->is('report/products*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('report/products') }}">
             <i class="fas fa-file-alt"></i>
             <span>{{ __('Products') }}</span>
@@ -60,14 +60,14 @@
     @if (Auth::user()->hasRole('Super Admin'))
         <li class="menu-header">{{ __('System') }}</li>
 
-        <li>
+        <li class="{{ request()->is('system/users*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ url('system/users') }}">
                 <i class="fas fa-user-cog"></i>
                 <span>{{ __('Users') }}</span>
             </a>
         </li>
 
-        <li>
+        <li class="{{ request()->is('system/ip-addr*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ url('system/ip-addr') }}">
                 <i class="fas fa-network-wired"></i>
                 <span>{{ __('IP Addresses') }}</span>
