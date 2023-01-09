49f841b2398de9fe561eb9aea86d6bc83891867d
Zain Adam <zainadam.id@gmail.com>
2023-01-10 00:16:06 +0700

    fix: unsupported version of daterangepicker

--- resources/views/components/assets/_datepicker.blade.php
+++ resources/views/components/assets/_datepicker.blade.php
@@ -1,8 +1,12 @@
 @push('css-lib')
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/2.1.27/daterangepicker.min.css"
+        integrity="sha512-6JT7OZMqeslSapllGvzgu4aLo8sGToaiUkabhIkoZgDHSCIVdGfpEzqdgJu7p1bMGQ14re+MBUfKig0JzT5WcQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endpush
 
 @push('js-lib')
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/2.1.27/daterangepicker.min.js"
+        integrity="sha512-d954S1Yc34Z+zNUtvoomoCtG1lCzfoENszxOXmqB784muP6G6qEXCug2HXLFn93H05wr3jfHTQwDdLQrUtXxrQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @endpush
