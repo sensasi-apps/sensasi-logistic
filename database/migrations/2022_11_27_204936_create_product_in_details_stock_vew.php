9f443ba1370173d8873ce95a67c344e63dcc8b2f
Zain Adam <zainadam.id@gmail.com>
2023-01-23 22:34:16 +0700

    refactor: stock view SQL

--- database/migrations/2022_11_27_204936_create_product_in_details_stock_vew.php
+++ database/migrations/2022_11_27_204936_create_product_in_details_stock_vew.php
@@ -12,15 +12,25 @@ class CreateProductInDetailsStockVew extends Migration
      */
     public function up()
     {
-        DB::connection('mysql')->statement('CREATE OR REPLACE VIEW product_in_details_stock_view AS
+        DB::connection('mysql')->statement('CREATE
+            OR REPLACE VIEW product_in_details_stock_view AS
             SELECT
                 pid.id as product_in_detail_id,
-                COALESCE(SUM(IF(`pi`.`deleted_at` IS NULL, `pid`.`qty`, 0)),0)/COUNT(`pid`.`id`) - COALESCE(SUM(IF(`mo`.deleted_at IS NULL, `mod`.`qty`, 0)),0) as qty
-            FROM product_in_details AS `pid`
-            LEFT JOIN product_out_details AS `mod` ON pid.id = `mod`.product_in_detail_id
-            LEFT JOIN product_outs AS `mo` ON `mod`.product_out_id = `mo`.id
-            LEFT JOIN product_ins AS `pi` ON `pid`.product_in_id = `pi`.id
-            GROUP BY pid.id, pid.qty;
+                -- qty in - qty out
+                COALESCE(
+                    IF(`pi`.`deleted_at` IS NULL, `pid`.`qty`, 0),
+                    0
+                ) - COALESCE(
+                    SUM(IF(`po`.deleted_at IS NULL, `pod`.`qty`, 0)),
+                    0
+                ) as qty
+            FROM
+                product_in_details AS `pid`
+                LEFT JOIN product_out_details AS `pod` ON pid.id = `pod`.product_in_detail_id
+                LEFT JOIN product_outs AS `po` ON `pod`.product_out_id = `po`.id
+                LEFT JOIN product_ins AS `pi` ON `pid`.product_in_id = `pi`.id
+            GROUP BY
+                pid.id;
         ');
     }
 
