938ef969a3d175f308c5dd460ab117fa7035b14e
Zain Adam <zainadam.id@gmail.com>
2023-02-18 23:09:58 +0700

    chore: add `getSuccessCrudResponse` on `Helper`

--- app/Helper.php
+++ app/Helper.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\UserActivity;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Jenssegers\Agent\Agent;
 
 /**
@@ -8,15 +10,11 @@ use Jenssegers\Agent\Agent;
  */
 class Helper
 {
-	public static function logAction(string $action, mixed $modelInstance)
+	public static function logAction(string $action, mixed $modelInstance): UserActivity
 	{
-		if (app()->environment('testing')) {
-			return;
-		}
-
 		$agent = new Agent();
 
-		UserActivity::create([
+		return UserActivity::create([
 			'user_id' => auth()->id(),
 			'action' => $action,
 			'model' => $modelInstance::class,
@@ -29,7 +27,7 @@ class Helper
 		]);
 	}
 
-	public static function logAuth(string $action)
+	public static function logAuth(string $action): UserActivity
 	{
 		$agent = new Agent();
 
@@ -40,6 +38,25 @@ class Helper
 			'browser' => $agent->browser() . ' ' . $agent->version($agent->browser()),
 			'device' => $agent->device(),
 			'os' => $agent->platform() . ' ' . $agent->version($agent->platform())
+		]);
+	}
+
+	public static function getSuccessCrudResponse(string $event, string $dataType, string $id_for_human): JsonResponse|RedirectResponse
+	{
+		$message = __("notification.data_{$event}", ['type' => $dataType, 'name' => "<b>{$id_for_human}</b>"]);
+		$color = $event == 'deleted' ? 'warning' : 'success';
+
+		if (request()->wantsJson()) {
+			return response()->json([
+				'notifications' => [[
+					'messageHtml' => $message,
+					'color' => $color
+				]]
+			]);
+		}
+
+		return back()->with('notifications', [
+			[$message, $color]
 		]);
 	}
 }
