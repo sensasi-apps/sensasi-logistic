319af4ee953c4b2e948904d883f75cecd41e04d9
Zain Adam <zainadam.id@gmail.com>
2023-02-23 15:32:30 +0700

    fix: obj is not queryBuilder in datatable api

--- app/Http/Controllers/Api/Datatable2Controller.php
+++ app/Http/Controllers/Api/Datatable2Controller.php
@@ -3,25 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Yajra\DataTables\Facades\DataTables;
 
 class Datatable2Controller extends Controller
 {
-	public function __invoke(string $modelName, string $params_json)
+	public function __invoke(string $modelName, string $params_json): null|JsonResponse
 	{
 		if (request()->ajax()) {
 			$modelClass = "App\Models\\$modelName";
 			$params = json_decode($params_json, true);
 
-			$queryBuilder = new $modelClass;
-
-			if (isset($params['with'])) {
-				$queryBuilder = $queryBuilder->with($params['with']);
-			}
-
-			if (!request()->order) {
-				$queryBuilder->orderByDesc('id');
-			}
+			$queryBuilder = $modelClass::with($params['with'] ?? []);
 
 			// TODO: fix search by date is reversed like 2020-01-01 to 01-01-2020
 			$tmp = DataTables::eloquent($queryBuilder);
