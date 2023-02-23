cb4b7e5797490b0c09a56e370ce0b20d7194a3b4
Zain Adam <zainadam.id@gmail.com>
2023-02-23 15:14:46 +0700

    fix: manufacture unique ids

--- database/factories/ManufactureFactory.php
+++ database/factories/ManufactureFactory.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ManufactureFactory extends Factory
 {
+    private $usedMaterialOutIds = [];
+    private $usedProductInIds = [];
+    private $materialOutIds;
+    private $productInIds;
+
     /**
      * Define the model's default state.
      *
@@ -13,14 +18,31 @@ class ManufactureFactory extends Factory
      */
     public function definition()
     {
-        $productInIds = \App\Models\ProductIn::all()->pluck('id');
-        $materialOutIds = \App\Models\MaterialOut::all()->pluck('id');
+        if (!$this->materialOutIds) {
+            // TODO: do this cache for all factories
+            $this->materialOutIds = \App\Models\MaterialOut::all()->pluck('id');
+        }
+
+        if (!$this->productInIds) {
+            $this->productInIds = \App\Models\ProductIn::all()->pluck('id');
+        }
+
+        do {
+            $materialOutId = $this->faker->randomElement($this->materialOutIds);
+        } while (in_array($materialOutId, $this->usedMaterialOutIds));
+
+        do {
+            $productInId = $this->faker->randomElement($this->productInIds);
+        } while (in_array($productInId, $this->usedProductInIds));
+
+        array_push($this->usedMaterialOutIds, $materialOutId);
+        array_push($this->usedProductInIds, $productInId);
 
         return [
             'code' => $this->faker->unique()->numerify('MAN-#####'),
             'at' => $this->faker->dateTimeBetween('-3 months', '-1 week'),
-            'material_out_id' => $this->faker->randomElement($materialOutIds),
-            'product_in_id' => $this->faker->randomElement($productInIds)            
+            'material_out_id' => $materialOutId,
+            'product_in_id' => $productInId
         ];
     }
 }
