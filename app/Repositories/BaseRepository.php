71200e0ac813585d00310d647839e890725b69c5
Zain Adam <zainadam.id@gmail.com>
2023-02-28 02:03:07 +0700

    chore: add base class for repo

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Route;
+
+class BaseRepository
+{
+	protected string $modelClass;
+	protected string $urlParamName;
+	protected Model $workingInstance;
+	protected array $withs;
+
+	public function __construct(Model $model = null)
+	{
+		$this->workingInstance = $model ?? $this->retrieveModel();
+	}
+
+	private function retrieveModel(): Model
+	{
+		if (Route::current() == null) {
+			return new $this->modelClass;
+		}
+
+		$id = Route::current()->parameters[$this->urlParamName] ?? null;
+
+		if ($id === null) {
+			return new $this->modelClass;
+		}
+
+		return $this->modelClass::with($this->withs)->findOrFail($id);
+	}
+}
