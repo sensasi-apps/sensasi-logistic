45db385cddedfe855563fd45d79dfd51c56ada10
Zain Adam <zainadam.id@gmail.com>
2023-03-02 23:31:33 +0700

    chore: translate menu

--- resources/views/layouts/components/_sidebar-menu-ul.blade.php
+++ resources/views/layouts/components/_sidebar-menu-ul.blade.php
@@ -1,77 +1,77 @@
-<ul class="sidebar-menu">
-    <li class="{{ request()->is('dashboard*') ? 'active' : '' }}">
+<ul class="sidebar-menu text-capitalize">
+    <li @class(['active' => request()->is('dashboard*')])>
         <a class="nav-link" href="{{ url('dashboard') }}">
             <i class="fas fa-chart-line"></i>
-            <span>{{ __('Dashboard') }}</span>
+            <span>{{ __('dashboard') }}</span>
         </a>
     </li>
 
     <li class="menu-header">Data</li>
 
-    <li class="{{ request()->is('materials*') ? 'active' : '' }}">
+    <li @class(['active' => request()->is('materials*')])>
         <a class="nav-link" href="{{ url('materials') }}">
             <i class="fas fa-pallet"></i>
-            <span>{{ __('Materials') }}</span>
+            <span>{{ __('materials') }}</span>
         </a>
     </li>
 
     @if (Auth::user()->hasRole('Manufacturer|Super Admin'))
-        <li class="{{ request()->is('manufactures*') ? 'active' : '' }}">
+        <li @class(['active' => request()->is('manufactures*')])>
             <a class="nav-link" href="{{ url('manufactures') }}">
                 <i class="fas fa-boxes"></i>
-                <span>{{ __('Manufactures') }}</span>
+                <span>{{ __('manufactures') }}</span>
             </a>
         </li>
     @endif
 
-    <li class="{{ request()->is('products*') ? 'active' : '' }}">
+    <li @class(['active' => request()->is('manufactures*')])>
         <a class="nav-link" href="{{ url('products') }}">
             <i class="fas fa-box"></i>
-            <span>{{ __('Products') }}</span>
+            <span>{{ __('products') }}</span>
         </a>
     </li>
 
 
 
-    <li class="menu-header">{{ __('Report') }}</li>
+    <li class="menu-header">{{ __('report') }}</li>
 
-    <li class="{{ request()->is('report/materials*') ? 'active' : '' }}">
+    <li @class(['active' => request()->is('report/manufactures*')])>
         <a class="nav-link" href="{{ url('report/materials') }}">
             <i class="fas fa-file-alt"></i>
-            <span>{{ __('Materials') }}</span>
+            <span>{{ __('materials') }}</span>
         </a>
     </li>
 
-    <li class="{{ request()->is('report/manufactures*') ? 'active' : '' }}">
+    <li @class(['active' => request()->is('report/manufactures*')])>
         <a class="nav-link" href="{{ url('report/manufactures') }}">
             <i class="fas fa-file-alt"></i>
-            <span>{{ __('Manufactures') }}</span>
+            <span>{{ __('manufactures') }}</span>
         </a>
     </li>
 
-    <li class="{{ request()->is('report/products*') ? 'active' : '' }}">
+    <li @class(['active' => request()->is('report/products*')])>
         <a class="nav-link" href="{{ url('report/products') }}">
             <i class="fas fa-file-alt"></i>
-            <span>{{ __('Products') }}</span>
+            <span>{{ __('products') }}</span>
         </a>
     </li>
 
 
     @if (Auth::user()->hasRole('Super Admin'))
-        <li class="menu-header">{{ __('System') }}</li>
+        <li class="menu-header">{{ __('system') }}</li>
 
-        <li class="{{ request()->is('system/users*') ? 'active' : '' }}">
+        <li @class(['active' => request()->is('report/users*')])>
             <a class="nav-link" href="{{ url('system/users') }}">
                 <i class="fas fa-user-cog"></i>
-                <span>{{ __('Users') }}</span>
+                <span>{{ __('users') }}</span>
             </a>
         </li>
 
-        <li class="{{ request()->is('system/ip-addr*') ? 'active' : '' }}">
+        {{-- <li class="{{ request()->is('system/ip-addr*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ url('system/ip-addr') }}">
                 <i class="fas fa-network-wired"></i>
                 <span>{{ __('IP Addresses') }}</span>
             </a>
-        </li>
+        </li> --}}
     @endif
 </ul>
