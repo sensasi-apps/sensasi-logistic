94fc3bf2d1775b26267893b2155b3028cf4139cf
Zain Adam <zainadam.id@gmail.com>
2023-03-18 18:16:34 +0700

    chore: add new `material_manufactures` table

---
+++ database/factories/MaterialManufactureFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MaterialOut;
+use App\Models\MaterialIn;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MaterialManufactureFactory extends Factory
+{
+    private array $usedMaterialOutIds = [];
+    private array $usedMaterialInIds = [];
+
+    private array $materialOutIds = [];
+    private array $materialInIds = [];
+
+    public function definition(): array
+    {
+        if (!$this->materialOutIds) {
+            $this->materialOutIds = MaterialOut::doesntHave('manufacture')->get()->pluck('id')->toArray();
+        }
+
+        if (!$this->materialInIds) {
+            $this->materialInIds = MaterialIn::all()->pluck('id')->toArray();
+        }
+
+        do {
+            $materialOutId = $this->faker->randomElement($this->materialOutIds);
+        } while (in_array($materialOutId, $this->usedMaterialOutIds));
+
+        do {
+            $materialInId = $this->faker->randomElement($this->materialInIds);
+        } while (in_array($materialInId, $this->usedMaterialInIds));
+
+        array_push($this->usedMaterialOutIds, $materialOutId);
+        array_push($this->usedMaterialInIds, $materialInId);
+
+        return [
+            'code' => $this->faker->unique()->numerify('MMAN-#####'),
+            'at' => $this->faker->dateTimeBetween('-3 months', '-1 week'),
+            'note' => $this->faker->sentence(10),
+            'material_out_id' => $materialOutId,
+            'material_in_id' => $materialInId
+        ];
+    }
+}
---
+++ database/migrations/2023_03_16_121700_create_material_manufactures_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('material_manufactures', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 15)->nullable()->unique();
+            $table->dateTime('at');
+            $table->text('note')->nullable();
+            $table->foreignId('material_out_id')->unique()
+                ->constrained('material_outs')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+            $table->foreignId('material_in_id')->unique()
+                ->constrained('material_ins')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('material_manufactures');
+    }
+};
